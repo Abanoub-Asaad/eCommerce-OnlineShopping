28a30a6a665f6ba19d4021ebfbc9c22f6b3162b1
Abanoub-Asaad <abanoub.asaad200010@gmail.com>
2020-08-01 15:49:18 +0200

    Update Main Page
    Add buttons: Join Now & Login

--- ECommerce/app/src/main/java/com/example/e_commerce/MainActivity.java
+++ ECommerce/app/src/main/java/com/example/e_commerce/MainActivity.java
@@ -3,16 +3,23 @@ package com.example.e_commerce;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button joinNowButton, loginbutton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Toast.makeText(MainActivity.this, "Firebase is working !!!",Toast.LENGTH_LONG).show();
+        Toast.makeText(MainActivity.this, "Made by: Abanoub Asaad",Toast.LENGTH_LONG).show();
+
+        joinNowButton = (Button) findViewById(R.id.main_join_now_btn);
+        loginbutton = (Button) findViewById(R.id.main_login_btn);
+
 
     }
 }
