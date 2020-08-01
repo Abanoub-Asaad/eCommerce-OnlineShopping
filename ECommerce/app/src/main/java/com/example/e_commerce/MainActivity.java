80bb6cceb5227fa28fc171a313c9a2ba6c93f9d8
Abanoub-Asaad <abanoub.asaad200010@gmail.com>
2020-08-01 13:29:01 +0200

    Add first first commit

---
+++ ECommerce/app/src/main/java/com/example/e_commerce/MainActivity.java
@@ -0,0 +1,18 @@
+package com.example.e_commerce;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Toast.makeText(MainActivity.this, "Firebase is working !!!",Toast.LENGTH_LONG).show();
+
+    }
+}
