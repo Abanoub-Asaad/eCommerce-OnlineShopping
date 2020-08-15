d37f7a3282b0ae856d2a087b396153d7039b5b21
Abanoub-Asaad <abanoub.asaad200010@gmail.com>
2020-08-15 14:15:22 +0200

    Save all chosen products and its data in Database

--- ECommerce/app/src/main/java/com/example/e_commerce/ProductDetailsActivity.java
+++ ECommerce/app/src/main/java/com/example/e_commerce/ProductDetailsActivity.java
@@ -1,21 +1,32 @@
 package com.example.e_commerce;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.bumptech.glide.Glide;
 import com.cepheuen.elegantnumberbutton.view.ElegantNumberButton;
 import com.example.e_commerce.Model.Products;
+import com.example.e_commerce.Prevalent.Prevalent;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 import com.squareup.picasso.Picasso;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.HashMap;
 
 public class ProductDetailsActivity extends AppCompatActivity {
 
@@ -49,10 +60,6 @@ public class ProductDetailsActivity extends AppCompatActivity {
         });
     }
 
-    private void AddProductToCartList(){
-
-    }
-
     private void getProductDetails(String productID) {
         DatabaseReference productsRef = FirebaseDatabase.getInstance().getReference().child("Products");
 
@@ -66,7 +73,7 @@ public class ProductDetailsActivity extends AppCompatActivity {
                     productPrice.setText(products.getProduct_price());
                     productDescription.setText(products.getProduct_description());
                     Picasso.get().load(products.getProduct_image()).into(productImage);
-
+                    //Glide.with(ProductDetailsActivity.this).load(products.getProduct_image()).into(productImage);
                 }
             }
 
@@ -75,6 +82,52 @@ public class ProductDetailsActivity extends AppCompatActivity {
 
             }
         });
+    }
+
+    private void AddProductToCartList(){
+
+        Calendar calendar = Calendar.getInstance();
+
+        SimpleDateFormat currentDate = new SimpleDateFormat("MMM dd, yyyy");
+        String saveCurrentDate = currentDate.format(calendar.getTime());
+
+        SimpleDateFormat currentTime = new SimpleDateFormat("HH:mm:ss a");
+        String saveCurrentTime = currentTime.format(calendar.getTime());
+
+        final DatabaseReference CartListRef = FirebaseDatabase.getInstance().getReference().child("Cart List");
+
+        final HashMap<String, Object> cartMap = new HashMap<>();
+        cartMap.put("product_id", productID);
+        cartMap.put("product_name", productName.getText().toString());
+        cartMap.put("product_description", productDescription.getText().toString());
+        cartMap.put("product_price", productPrice.getText().toString());
+        cartMap.put("date", saveCurrentDate);
+        cartMap.put("time", saveCurrentTime);
+        cartMap.put("quantity", numberBtn.getNumber());
+        cartMap.put("discount", "");
+
+        CartListRef.child("User View").child(Prevalent.currentOnlineUser.getPhone()).child("Products")
+                .child(productID).updateChildren(cartMap)
+                .addOnCompleteListener(new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        if(task.isSuccessful()){
+                            CartListRef.child("Admin View").child(Prevalent.currentOnlineUser.getPhone()).child("Products")
+                                    .child(productID).updateChildren(cartMap)
+                                    .addOnCompleteListener(new OnCompleteListener<Void>() {
+                                        @Override
+                                        public void onComplete(@NonNull Task<Void> task) {
+                                            if(task.isSuccessful()){
+                                                Toast.makeText(ProductDetailsActivity.this, "Added to Cart List", Toast.LENGTH_SHORT).show();
+
+                                                Intent intent = new Intent(ProductDetailsActivity.this, HomeActivity.class);
+                                                startActivity(intent);
+                                            }
+                                        }
+                                    });
+                        }
+                    }
+                });
     }
 }
 
