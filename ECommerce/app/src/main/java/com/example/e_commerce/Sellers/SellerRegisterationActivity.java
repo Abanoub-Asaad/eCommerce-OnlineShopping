b0e19e56f17d0be33d3598f671f5108250ef327d
Abanoub-Asaad <abanoub.asaad200010@gmail.com>
2020-09-15 18:52:11 +0200

    connect Seller registeration to Database

--- ECommerce/app/src/main/java/com/example/e_commerce/Sellers/SellerRegisterationActivity.java
+++ ECommerce/app/src/main/java/com/example/e_commerce/Sellers/SellerRegisterationActivity.java
@@ -1,5 +1,6 @@
 package com.example.e_commerce.Sellers;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.ProgressDialog;
@@ -13,17 +14,28 @@ import android.widget.Toast;
 
 import com.example.e_commerce.R;
 import com.example.e_commerce.RegisterActivity;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.HashMap;
 
 public class SellerRegisterationActivity extends AppCompatActivity {
 
     private ProgressDialog loadingBar;
     private EditText sellerName, sellerPhone, sellerEmail, sellerPassword, sellerAddress;
     private Button register, login;
+    private FirebaseAuth auth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_seller_registeration);
+
+        auth = FirebaseAuth.getInstance();
 
         sellerName= findViewById(R.id.seller_name);
         sellerPhone = findViewById(R.id.seller_phone);
@@ -33,6 +45,8 @@ public class SellerRegisterationActivity extends AppCompatActivity {
 
         register = findViewById(R.id.seller_register_btn);
         login = findViewById(R.id.seller_have_account);
+
+        loadingBar = new ProgressDialog(this);
 
         register.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -51,11 +65,11 @@ public class SellerRegisterationActivity extends AppCompatActivity {
     }
 
     private void createSellerAccount() {
-        String name = sellerName.getText().toString();
-        String phone = sellerPhone.getText().toString();
-        String email = sellerEmail.getText().toString();
-        String password = sellerPassword.getText().toString();
-        String address = sellerAddress.getText().toString();
+        final String name = sellerName.getText().toString();
+        final String phone = sellerPhone.getText().toString();
+        final String email = sellerEmail.getText().toString();
+        final String password = sellerPassword.getText().toString();
+        final String address = sellerAddress.getText().toString();
 
         if (TextUtils.isEmpty(name))
             Toast.makeText(this, "Please write your name", Toast.LENGTH_SHORT).show();
@@ -70,17 +84,46 @@ public class SellerRegisterationActivity extends AppCompatActivity {
         else if (TextUtils.isEmpty(address))
             Toast.makeText(this, "Please write your address", Toast.LENGTH_SHORT).show();
         else{
+
             loadingBar = new ProgressDialog(SellerRegisterationActivity.this);
-            loadingBar.setTitle("Create Account");
+            loadingBar.setTitle("Create Seller Account");
             loadingBar.setMessage("Please wait, while we are checking the credentials");
             loadingBar.setCanceledOnTouchOutside(false);
             loadingBar.show();
 
-            validatePhoneNumber(name, phone, email, password, address);
+            auth .createUserWithEmailAndPassword(email, password)
+                    .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if(task.isSuccessful()){
+                                final DatabaseReference rootRef = FirebaseDatabase.getInstance().getReference();
+
+                                String sid = auth.getCurrentUser().getUid();
+
+                                HashMap<String, Object> sellerMap = new HashMap<>();
+
+                                sellerMap.put("seller_id", sid);
+                                sellerMap.put("name", name);
+                                sellerMap.put("phone", phone);
+                                sellerMap.put("email", email);
+                                sellerMap.put("password", password);
+                                sellerMap.put("address", address);
+
+                                rootRef.child("Sellers").child(sid).updateChildren(sellerMap)
+                                        .addOnCompleteListener(new OnCompleteListener<Void>() {
+                                            @Override
+                                            public void onComplete(@NonNull Task<Void> task) {
+                                                loadingBar.dismiss();
+
+                                                Intent intent = new Intent(SellerRegisterationActivity.this, SellerLoginActivity.class);
+                                                startActivity(intent);
+
+                                                Toast.makeText(SellerRegisterationActivity.this, "You are registered successfully", Toast.LENGTH_SHORT).show();
+                                            }
+                                        });
+                            }
+                        }
+                    });
         }
-    }
-
-    private void validatePhoneNumber(String name, String phone, String email, String password, String address) {
-
     }
 }
